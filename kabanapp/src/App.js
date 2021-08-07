01e066660ada96dee85b846de2a8a1f19126b7c7
PalomaReb <paloma.rebolleda@gmail.com>
2021-08-07 13:53:50 +0200

    prop header

--- kabanapp/src/App.js
+++ kabanapp/src/App.js
@@ -1,13 +1,12 @@
 // import { Divider } from '@material-ui/core';
 import TemplateContainer from './components/All-Columns/ContainerTemplate';
 import './App.css';
-// import TemplateContainer from './componentContainerTemplate/ContainerTemplate.jsx';
 import TemplateHeader from './components/Header/HeaderTemplate';
 
 function App() {
   return (
     <div>
-      <TemplateHeader></TemplateHeader>
+      <TemplateHeader updatedLast=" "></TemplateHeader>
       <TemplateContainer></TemplateContainer>
     </div>
   );
--- kabanapp/src/components/Header/HeaderTemplate.jsx
+++ kabanapp/src/components/Header/HeaderTemplate.jsx
@@ -1,28 +1,30 @@
-import './HeaderTemplate.css';
+import "./HeaderTemplate.css";
 
-
-function TemplateHeader(){
-
-    return(
-        <div className="mainContainer">
-        <div className="headerContainer">
-          <img className="headerLogo" src=" https://www.neoland.es/hubfs/favicon%20neoland-02-02-02.png" alt="LogoNeoland" />
-          <p className="headerText">Version 1.0</p>
-        </div>
-        
-        <div className="searchContainer">
-          <div className="info">
-            <p><b>Version 1.0</b></p>
-            <p>Updated on 12 abr</p>
-          </div>
-          <div className="filter">
-            <input type="text" placeholder="Filter cards"></input>
-          </div>
-        </div>
-  
-  
+function TemplateHeader(props) {
+  return (
+    <div className="mainContainer">
+      <div className="headerContainer">
+        <img
+          className="headerLogo"
+          src=" https://www.neoland.es/hubfs/favicon%20neoland-02-02-02.png"
+          alt="LogoNeoland"
+        />
+        <p className="headerText">Version 1.0</p>
       </div>
-    )
+
+      <div className="searchContainer">
+        <div className="info">
+          <p>
+            <b>Version 1.0</b>
+          </p>
+          <p>{props.updatedLast}</p>
+        </div>
+        <div className="filter">
+          <input type="text" placeholder="Filter cards"></input>
+        </div>
+      </div>
+    </div>
+  );
 }
- 
-    export default TemplateHeader;
+
+export default TemplateHeader;
